c584922c1f321a580dd216b4c1915f3f05b50f8d
Denys <bakhonin1@gmail.com>
2024-04-21 19:08:59 +0200

    hw02-express

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,1 +1,49 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import crypto from "node:crypto";
 
+const contactsPath = path.resolve("db", "contacts.json");
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+};
+
+export const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  return result || null;
+};
+
+export const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+export const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: crypto.randomUUID(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+export const updateContactId = async (id, data) => {
+  const contact = await listContacts();
+  const index = contact.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contact[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+  return contact[index];
+};
