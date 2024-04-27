db779fb9d64e0669d1b7a6b87465d0b0d7117bf0
Denys <bakhonin1@gmail.com>
2024-04-27 18:38:13 +0200

    add port 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import app from "./app.js";
 
 const { DB_HOST } = process.env;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.set("strictQuery", true);
 
